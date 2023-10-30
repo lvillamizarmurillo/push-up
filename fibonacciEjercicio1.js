39c7909f2464e43150576ed79bc023c6c4dbda0a
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-10-30 15:21:38 -0500

    feat: :sparkles: Ejercicio 3 de fubonacci
    The exercise three of the numbers fubonacci sinish
    
    }

---
+++ fibonacciEjercicio1.js
@@ -0,0 +1,11 @@
+//ejercicio 1
+let lista = [0, 1]
+let resultado = 0
+let n = 11
+//let n = parseInt(prompt('Digite un número'))
+for (let i = 1; i < n; i++){
+    resultado = lista[(i-1)]
+    resultado = resultado + lista[i]
+    lista.push(resultado)
+}
+console.log(lista[n]);
