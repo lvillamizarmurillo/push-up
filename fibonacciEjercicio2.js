3142fc3f4969ec42b3c920efa44a76476d949b4d
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-10-30 15:40:02 -0500

    feat: :sparkles: Exercise two of fubonacci
    The exercise two of fubonacci is finish

---
+++ fibonacciEjercicio2.js
@@ -0,0 +1,17 @@
+//ejercicio 1
+let lista = [0, 1]
+let resultado = 0
+let contador = 0
+let n = 21
+//let n = parseInt(prompt('Digite un número'))
+while (n > lista[contador]) {
+    contador = contador+1
+    resultado = lista[(contador - 1)]
+    resultado = resultado + lista[contador]
+    lista.push(resultado)
+}
+if (n == lista[contador]) {
+    console.log('El numero '+n+', si forma parte de los numeros fubonacci\n:)');
+} else {
+    console.log('El numero ' + n + ', no forma parte de los numeros fubonacci\n:(');
+}
